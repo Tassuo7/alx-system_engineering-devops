27afcb08593e54ac7b1da4c4b5e505072f6e583d
Tassuo7 <abdelhamid.jack@gmail.com>
2023-08-27 07:43:29 -0700

    advanced

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,10 @@
+#  SSH client configuration 
+file_line { 'Turn off passwd auth':
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/school',
+}
