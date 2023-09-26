6584ca7b6da9c4936266365ea23abc0c60aa8024
Tassuo7 <abdelhamid.jack@gmail.com>
2023-09-25 17:03:54 -0700

    json

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Exports todo list information for a given employee ID in the JSON format
+"""
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+    uid = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    userId = requests.get(url + "users/{}".format(uid)).json()
+    todos = requests.get(url + "todos", params={"userId": uid}).json()
+    un = userId.get("username")
+
+    with open("{}.json".format(uid), "w") as jsonfile:
+        for t in todos:
+            json.dump({uid: [{
+                "task": t["title"], "completed": t["completed"], "username": un
+                }]}, jsonfile)
