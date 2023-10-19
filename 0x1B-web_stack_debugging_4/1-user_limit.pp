e3980b13f2b3149b2ee031ab34bb1875b499e644
Tassuo7 <abdelhamid.jack@gmail.com>
2023-10-19 10:22:14 -0700

    fix

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,11 +1,11 @@
 # add the holborton user and increase amount of files to open
 exec { 'increase-file-limit':
   provider => shell,
-  command => 'sudo sed -i "s/nofile 5/nofile 50000/" /etc/security/limits.conf',
+  command => 'sudo sed -i "/holberton hard/s/5/8192/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
 
 exec { 'increase-file-limits2':
-  command => 'sudo sed -i "s/nofile 4/nofile 40000/" /etc/security/limits.conf',
+  command => 'sudo sed -i "/holberton hard/s/4/8192/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
